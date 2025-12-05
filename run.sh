a2e5602d51da7f9b7d3fb551e1428f28c9136c4d
lynlynnie <carolynalcaraz2711@hotmail.com>
2025-12-05 17:09:50 +0100

    Fixed model 2

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+set -e
+
 if [ ! -f "output/preprocessor.joblib" ]; then
     echo "Preprocessor not found - generating..."
     python src/text_preprocessing.py
 fi
 
-python src/serve_model.py
+echo "Starting model service..."
+exec python src/serve_model.py
